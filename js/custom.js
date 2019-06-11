1f88cef77bea574343b8e5da7b6b035f5eaf26ad
Mateus Fernandes <mateusfernandes12@live.com>
2019-06-11 13:34:58 -0300

    lazyu

--- js/custom.js
+++ js/custom.js
@@ -37,3 +37,9 @@ var aText = new Array(
     
     
     typewriter();
+    // plugin build-in placeholder management
+    $(function() {
+     $('.lazy').lazy({
+         placeholder: "data:image/gif;base64,R0lGODlhEALAPQAPzl5uLr9Nrl8e7..."
+     });
+ });
